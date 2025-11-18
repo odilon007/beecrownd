c8d3f1aaff80eae56153461652801ac8617e191a
Odilon Francelino de Pontes Neto <odilonpontes7@gmail.com>
2025-11-18 18:30:11 -0300

    Create ex3303.cpp

---
+++ cpp/ex3303.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string palavra;
+    cin >> palavra;
+    if (palavra.size() >=10) {
+        cout << "palavrao\n";
+    } else {
+        cout << "palavrinha\n";
+    }
+    return 0;
+}
