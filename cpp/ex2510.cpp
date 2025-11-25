5906d5b75ffa2d75698be4dc0d97ffa3c810451a
Odilon Francelino de Pontes Neto <odilonpontes7@gmail.com>
2025-11-25 08:07:30 -0300

    Add basic input/output program in ex2510.cpp

---
+++ cpp/ex2510.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    string n;
+    
+    cin >> t;
+    for (int i=0; i<t; i++) {
+        cin >> n;
+        cout << "Y"<< endl;
+    }
+    
+    return 0;
+}
